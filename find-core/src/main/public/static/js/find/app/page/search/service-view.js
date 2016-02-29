b27c965256a3e3d8f10aed9c03b68f605d90ec57
Tung Jin Chew <tung-jin.chew@hp.com>
2016-02-29 16:40:41 +0000

    Fix merge

--- find-core/src/main/public/static/js/find/app/page/search/service-view.js
+++ find-core/src/main/public/static/js/find/app/page/search/service-view.js
@@ -125,7 +125,8 @@ define([
                 indexesCollection: this.indexesCollection,
                 queryModel: this.queryModel,
                 queryTextModel: this.queryState.queryTextModel,
-                parametricCollection: this.parametricCollection
+                parametricCollection: this.parametricCollection,
+                queryState: this.queryState
             };
 
             this.resultsViewAugmentation = new this.ResultsViewAugmentation({
