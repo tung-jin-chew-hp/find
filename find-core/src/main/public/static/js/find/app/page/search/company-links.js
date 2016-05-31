8fd708dcde424b73c94488de5eb735ad90535b84
Tung Jin Chew <tung-jin.chew@hp.com>
2016-05-31 11:00:42 +0100

    Disable company links

--- find-core/src/main/public/static/js/find/app/page/search/company-links.js
+++ find-core/src/main/public/static/js/find/app/page/search/company-links.js
@@ -7,17 +7,17 @@ define(function() {
 
     // Company links displayed on the splash page and thier logos
     return [
-        {company: 'IBM', image: 'ibm.jpg', alt: 'IBM'},
-        {company: 'INFORMATION BUILDERS', image: 'informationbuilders.png', alt: 'Information Builders'},
-        {company: 'MICROSOFT', image: 'microsoft.png', alt: 'Microsoft'},
-        {company: 'MICROSTRATEGY', image: 'microstrategy.png', alt: 'MicroStrategy'},
-        {company: 'OPENTEXT', image: 'opentext.png', alt: 'OpenText'},
-        {company: 'ORACLE', image: 'oracle.jpg', alt: 'Oracle'},
-        {company: 'QLIK', image: 'qlik.png', alt: 'Qlik'},
-        {company: 'SAP', image: 'sap.jpg', alt: 'SAP'},
-        {company: 'SAS', image: 'sas.jpg', alt: 'SAS'},
-        {company: 'TABLEAU', image: 'tableau.jpg', alt: 'Tableau Software'},
-        {company: 'TIBCO SOFTWARE', image: 'tibco.png', alt: 'TIBCO Software'}
+        //{company: 'IBM', image: 'ibm.jpg', alt: 'IBM'},
+        //{company: 'INFORMATION BUILDERS', image: 'informationbuilders.png', alt: 'Information Builders'},
+        //{company: 'MICROSOFT', image: 'microsoft.png', alt: 'Microsoft'},
+        //{company: 'MICROSTRATEGY', image: 'microstrategy.png', alt: 'MicroStrategy'},
+        //{company: 'OPENTEXT', image: 'opentext.png', alt: 'OpenText'},
+        //{company: 'ORACLE', image: 'oracle.jpg', alt: 'Oracle'},
+        //{company: 'QLIK', image: 'qlik.png', alt: 'Qlik'},
+        //{company: 'SAP', image: 'sap.jpg', alt: 'SAP'},
+        //{company: 'SAS', image: 'sas.jpg', alt: 'SAS'},
+        //{company: 'TABLEAU', image: 'tableau.jpg', alt: 'Tableau Software'},
+        //{company: 'TIBCO SOFTWARE', image: 'tibco.png', alt: 'TIBCO Software'}
     ];
 
 });
